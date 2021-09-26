a8f988afcdfb8978fc0089fc9d423b54de33bd41
GeoffreyXue <xuegeoff@gmail.com>
2021-09-26 17:34:45 -0500

    navigation update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,11 +12,12 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/website/">Home</Nav.Link>
+            <Nav.Link href="/website/projects">Projects</Nav.Link>
+            <Nav.Link href="/website/contact">Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
+        <Navbar.Text>V0.01</Navbar.Text>
         </Container>
     </Navbar>
   );
