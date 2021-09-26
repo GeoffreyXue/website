45c13a471d8850173f6b3bc4210eb043f3b66322
GeoffreyXue <xuegeoff@gmail.com>
2021-09-26 18:36:19 -0500

    Home page work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,20 @@
+import Image from 'react-bootstrap/Image';
+
+import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   return (
-    <div>Home</div> 
+    <div className='Home'>
+        <div className='Panels'>
+            <div className='TopPanel'/>
+            <div className='Strip'/>
+            <div className='BottomPanel'/>
+        </div>
+        <div className='PictureBackground'/>
+        <Image src='picture.png' className='Picture' roundedCircle/>
+        <img src='picture.png' />
+    </div> 
   );
 }
 
