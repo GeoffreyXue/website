aeb00dfcdeb5f9dc2c7bcedd601f82fbb561d3c8
GeoffreyXue <85135277+GeoffreyXue@users.noreply.github.com>
2021-12-31 00:52:55 -0600

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,7 +3,7 @@ import './Resume.css';
 export default function Resume() {
     return (
         <div className="Resume">
-            <iframe title="Resume" src="https://drive.google.com/file/d/12qkxYN-FQcCjt8-tPsMoA-m8ghZc7jhV/preview" height="100%" width="100%"></iframe>
+            <iframe title="Resume" src="https://drive.google.com/file/d/1f6tbueydUiYeupfHdiil2-1mEbw2eSh4/preview" height="100%" width="100%"></iframe>
         </div>
     )
 }
